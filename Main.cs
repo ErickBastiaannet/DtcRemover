fcc6ff5c7fd14bd90d2f9b76442d347ada033f13
ErickBastiaannet <81415764+ErickBastiaannet@users.noreply.github.com>
2022-12-01 14:00:29 +0100

    Add 8W0907115Q_0003

--- Main.cs
+++ Main.cs
@@ -264,6 +264,36 @@ namespace DtcRemover
                         MessageBox.Show("MG1CS111 Based on 81A907115A_0001 Algorithm Detected", "MG1CS111");
                     }
                 }
+                //Add 8W0907115Q_0003
+                if (potentialDFES_DTCO.Count != 1 || potentialDFES_Cls.Count != 1 || potentialDFC_DisblMsk2.Count != 1)
+                {
+                    //block length is 1346 8 bit, 2692 16 bit error codes.
+                    lengthErrorCodes8bit = 1346;
+                    lengthErrorCodes16bit = lengthErrorCodes8bit * 2;
+                    //Pcode Block
+                    //Start of DFES_DTCO 16 bit (DFES_DTCO.DFC_Unused_C) 
+                    DFES_DTCO = new byte[] { 00, 00, 22, 09, 10 };
+                    //Start of Fehlerklasse 8 bit
+                    DFES_Cls = new byte[] { 11, 01, 11, 11, 02 };
+                    //Start of DisableMask 16 bit
+                    DFC_DisblMsk2 = new byte[] { 255, 255, 03, 224 };
+
+                    //Find locations of DTC tables
+                    potentialDFES_DTCO = SearchBytePattern(DFES_DTCO, bytes);
+                    //Speed up the search proces by skipping the next algorithms when potentialDFES_DTCO is empty
+                    if (potentialDFES_DTCO.Count != 0)
+                    {
+                        potentialDFES_Cls = SearchBytePattern(DFES_Cls, bytes);
+                        potentialDFC_DisblMsk2 = SearchBytePattern(DFC_DisblMsk2, bytes);
+                    }
+
+                    //Show Messagebox with detected ECU Type
+                    if (potentialDFES_DTCO.Count == 1 && potentialDFES_Cls.Count == 1 && potentialDFC_DisblMsk2.Count == 1)
+                    {
+                        hiLoSwitch = true;
+                        MessageBox.Show("MG1CS001 Based on 8W0907115Q_0003 Algorithm Detected", "MG1CS001");
+                    }
+                }
                 //Add 8P0907115BA_0020
                 if (potentialDFES_DTCO.Count != 1 || potentialDFES_Cls.Count != 1 || potentialDFC_DisblMsk2.Count != 1)
                 {
